484cf08469c0fb73684d42c06d8c798718f0d65e
memomesqui <158315089+memomesqui@users.noreply.github.com>
2024-02-07 18:12:43 -0500

    made the ball move

---
+++ Pong CCNY Melody Sueros/Assets/Scenes/BallControll.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    //declare global variables- Wednesday
+    public Rigidbody2D rbBall;
+
+    public float force = 200;
+
+    private float xDir;
+    private float yDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello World!");
+
+        Vector3 direction = new Vector3(0, 0, 0);
+
+        xDir = Random.Range(0, 2);
+        //Debug.Log(" xDir = " + xDir);
+        if (xDir == 0)
+        {
+            direction.x = -1;
+        }
+        else if (xDir == 1)
+        {
+            direction.x = 1;
+        }
+
+        if (yDir == 0)
+        {
+            direction.y = -1;
+        }
+        else if (yDir == 1)
+        {
+            direction.y = 1;
+        }
+
+        //add force to start movement
+        rbBall.AddForce(direction * force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
