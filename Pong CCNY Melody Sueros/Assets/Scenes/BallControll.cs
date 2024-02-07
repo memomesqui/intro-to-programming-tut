ba2a9d2b288cea3cb65b45dd04b6ecd3e1073482
memomesqui <158315089+memomesqui@users.noreply.github.com>
2024-02-07 18:25:29 -0500

    more changes

--- Pong CCNY Melody Sueros/Assets/Scenes/BallControll.cs
+++ Pong CCNY Melody Sueros/Assets/Scenes/BallControll.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallController : MonoBehaviour
+public class BallControll : MonoBehaviour
 {
     //declare global variables- Wednesday
     public Rigidbody2D rbBall;
